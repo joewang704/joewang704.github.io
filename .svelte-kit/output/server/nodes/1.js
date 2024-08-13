24f1f6b1e44c76f8a03a988fbbb6f06a4b3a5415
Joe Wang <joewang704@gmail.com>
2024-08-13 16:19:27 -0400

    Try with new setup

---
+++ .svelte-kit/output/server/nodes/1.js
@@ -0,0 +1,8 @@
+
+
+export const index = 1;
+let component_cache;
+export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/1.e7G0IZBq.js","_app/immutable/chunks/scheduler.Ce_0Mfso.js","_app/immutable/chunks/index.CoaCY2RK.js","_app/immutable/chunks/stores.kUx3wf7h.js","_app/immutable/chunks/entry.D2gqwTVy.js","_app/immutable/chunks/index.M7WuUczP.js"];
+export const stylesheets = [];
+export const fonts = [];
